8098b09084ddc13b66c895643ff82fd52cae41a0
Alex Ciobanu <tsopi10@gmail.com>
2021-10-28 13:59:01 +0300

    Create main and welcome functions to start the app

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Main file
+"""
+
+def welcome():
+    """
+    Welcome function used to provide user with choice of admin/athlete sign in, or sign up.
+    """
+    print("Welcome to TRXtreme.\n")
+    print("To sign in as a user, please input the letter u and enter.")
+    print("To sign in as admin, please input the letter a and enter.")
+    print("To sign up, please input the letter s and enter.\n")
+    
+    user_answer = input()
+
+    if user_answer.lower() == "u":
+        sign_in()
+    elif user_answer.lower() == "a":
+        admin_sign_in()
+    elif user_answer.lower() == "s":
+        sign_up()
+    else:
+        
+
+
+def main():
+    """
+    Main function used to run all necessary program functions.
+    """
+

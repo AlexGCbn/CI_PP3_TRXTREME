76c76ac005da94befed325f78469be258b96026a
Alex Ciobanu <tsopi10@gmail.com>
2021-11-13 19:49:17 +0200

    Complete cleanup for gservices module

--- gservices.py
+++ gservices.py
@@ -6,9 +6,9 @@ import gspread
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 
-"""
-Scope required for Google Sheets, Calendar and Drive to work
-"""
+
+# Scope required for Google Sheets, Calendar and Drive to work
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -16,13 +16,20 @@ SCOPE = [
     "https://www.googleapis.com/auth/calendar",
 ]
 
-"""
-Global variables used for Google Sheets and Calendar
-"""
-CREDS = Credentials.from_service_account_file("creds.json")  # Credentials file
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)  # Scoped credentials
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)  # Google Sheets authorization
-SHEET = GSPREAD_CLIENT.open("trxtreme")  # Opens selected Google Sheet
-SHEET_ID = "1izyPTgGIt_uKegNn2I0lFUdrAuXPJisNeXgvzN2EG_g"  # The selected Google Sheet ID, used in some functions
-CALENDAR = build("calendar", "v3", credentials=CREDS)  # Google Calendar service build
-CALENDAR_ID = "trxtreme2021@gmail.com"  # Google Calendar ID
+
+# Global variables used for Google Sheets and Calendar
+
+# Credentials file
+CREDS = Credentials.from_service_account_file("creds.json")
+# Scoped credentials
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# Google Sheets authorization
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# Opens selected Google Sheet
+SHEET = GSPREAD_CLIENT.open("trxtreme")
+# The selected Google Sheet ID
+SHEET_ID = "1izyPTgGIt_uKegNn2I0lFUdrAuXPJisNeXgvzN2EG_g"
+# Google Calendar service build
+CALENDAR = build("calendar", "v3", credentials=CREDS)
+# Google Calendar ID
+CALENDAR_ID = "trxtreme2021@gmail.com"
